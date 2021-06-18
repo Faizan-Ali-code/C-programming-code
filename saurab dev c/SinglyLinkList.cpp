89f2d34db1ce7f83f306e1201f846aba0d71e2a5
Faizan-Ali-code <faizanali7273374@gmail.com>
2021-06-18 04:56:59 -0700

    made a linked list

---
+++ saurab dev c/SinglyLinkList.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+   class singly_link{
+	private:
+		struct node{
+			int data;
+			node* next;
+		};
+	    node* head = NULL;
+	
+	
+	};
+
+int main(){
+	
+	singly_link s;
+	
+	
+	return 0;
+}
+
---
+++ saurab dev c/codeSinglyLinkList.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+using namespace std;
+
+   class singly_link{
+	private:
+		struct node{
+			int data;
+			node* next_node_address;
+		};
+	    node* head = NULL;
+	public:
+		void insert();
+		
+	
+	};
+void singly_link::insert(int &n){
+	node* new_node_address = new node;
+	new_node_address -> data = n;
+	new_node_address -> next_node_address = NULL;
+	if(head == NULL){
+		head = new_node_address;
+	}
+	else{
+		node* ptr = head;
+		while(ptr -> next_node_address != NULL){
+			ptr = ptr -> next_node_address;
+		}
+	    ptr -> next_node_address = new_node_address
+	}
+}	
+
+int main(){
+	int n;
+	singly_link s;
+	cout<<"\n\n Enter Value : ";
+	cin>>n;
+	s.insert(n);
+	
+	
+	return 0;
+}
+
